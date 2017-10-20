1f026a72bcaa81170eaee539fb53e95c7f0374b6
DCay <atriumandvintrigol@gmail.com>
2017-10-20 14:09:17 +0300

    Revert "Deleted the MVC Framework"
    This reverts commit cb5d03c2637b89dfe1751c4e9c9447215bfd41e7.

---
+++ SoftUni-C#-Web-MVC-Framework/SimpleMvc.Domain/User.cs
@@ -0,0 +1,22 @@
+﻿namespace SimpleMvc.Domain
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+        public User()
+        {
+            this.Notes = new List<Note>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public ICollection<Note> Notes { get; set; }
+    }
+}
